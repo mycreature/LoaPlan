c862acd8e24ca0026aff532243e7a22661dbed50
Minseop Kim <aistjq1691@gmail.com>
2025-04-28 16:15:06 +0900

    [style] 간격 수정정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,24 +3,25 @@ import Block from '../components/Block'
 const Home = () => {
   return (
     <div className='min-h-screen bg-gray-600'>
-      <main className='mx-auto max-w-3xl p-[10px] text-center'></main>
-      <div className='flex items-center justify-center space-x-[10px] text-center'>
-        <Block width={250} height={300} />
-        <Block width={276} height={300} />
-        <Block width={458} height={300} />
-      </div>
-      <div className='flex items-center justify-center space-x-[10px] pt-4 text-center'>
-        <div className='grid min-w-[735px] grid-cols-2 gap-[10px]'>
-          <div className='col-span-2'>
-            <Block width={735} height={70} />
+      <main className='mx-[10px] pt-[10px] pb-5'>
+        <div className='flex items-center justify-center space-x-[10px] text-center'>
+          <Block width={250} height={300} />
+          <Block width={276} height={300} />
+          <Block width={458} height={300} />
+        </div>
+        <div className='flex items-center justify-center space-x-[10px] pt-[10px] text-center'>
+          <div className='grid min-w-[735px] grid-cols-2 gap-[10px]'>
+            <div className='col-span-2'>
+              <Block width={735} height={70} />
+            </div>
+            <Block width={362.5} height={500} />
+            <Block width={362.5} height={500} />
           </div>
-          <Block width={362.5} height={500} />
-          <Block width={362.5} height={500} />
+          <div className='w-[259px]'>
+            <Block width={259} height={580} />
+          </div>
         </div>
-        <div className='w-[259px]'>
-          <Block width={259} height={580} />
-        </div>
-      </div>
+      </main>
     </div>
   )
 }
