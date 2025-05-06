1b9633c75d56a3e61a34397cdd10ea2db0642d68
Minseop Kim <aistjq1691@gmail.com>
2025-05-06 18:06:40 +0900

    [feat] 캐릭터, 원정, 마켓 api 함수 추가

---
+++ src/api/apiService.ts
@@ -0,0 +1,42 @@
+import instance from './instance'
+
+export const getMarketData = async (itemName: string, itemCode: number) => {
+  try {
+    const res = await instance.post('markets/items', {
+      Sort: 'GRADE',
+      CategoryCode: itemCode,
+      ItemName: itemName,
+      SortCondition: 'ASC',
+    })
+    console.log(`✅ ${itemName} 아이템 호출`, res.data)
+  } catch (error) {
+    console.error(`❌ ${itemName} 호출 실패`, error)
+  }
+}
+
+export const getExpeditionData = async (Name: string) => {
+  try {
+    const res = await instance.get(`characters/${Name}/siblings`)
+    console.log(`✅ ${Name} 정보 호출`, res.data)
+  } catch (error) {
+    console.error(`❌ ${Name} 호출 실패`, error)
+  }
+}
+
+export const getCharInfoData = async (Name: string, Filter?: string) => {
+  try {
+    const res = await instance.get(`armories/characters/${Name}${Filter ? `/${Filter}` : ''}`)
+    console.log(`✅ ${Name} 정보 호출:`, res.data)
+  } catch (error) {
+    console.error(`❌ ${Name} 호출 실패:`, error)
+  }
+}
+
+export const getCharProfile = async (Name: string) => {
+  try {
+    const res = await instance.get(`armories/characters/${Name}/profiles`)
+    console.log(`✅ ${Name} 프로필 호출:`, res.data)
+  } catch (error) {
+    console.error(`❌ ${Name} 프로필 호출 실패:`, error)
+  }
+}
