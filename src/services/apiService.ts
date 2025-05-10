c3b328243ac63ec1e5596003c73198366a2a5752
Minseop Kim <aistjq1691@gmail.com>
2025-05-10 19:59:36 +0900

    [chore] api 기능 콘솔 최적화

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -18,7 +18,7 @@ export const getMarketData = async (itemName: string, itemCode: number) => {
 export const getExpeditionData = async (Name: string) => {
   try {
     const res = await instance.get(`characters/${Name}/siblings`)
-    console.log(`✅ ${Name} 정보 호출`, res.data)
+    console.log(`✅ ${Name} 정보 호출`)
     return res.data
   } catch (error) {
     console.error(`❌ ${Name} 호출 실패`, error)
@@ -28,7 +28,7 @@ export const getExpeditionData = async (Name: string) => {
 export const getCharInfoData = async (Name: string, Filter?: string) => {
   try {
     const res = await instance.get(`armories/characters/${Name}${Filter ? `/${Filter}` : ''}`)
-    console.log(`✅ ${Name} 정보 호출:`, res.data)
+    console.log(`✅ ${Name} 정보 호출`)
     return res.data
   } catch (error) {
     console.error(`❌ ${Name} 호출 실패:`, error)
@@ -38,7 +38,7 @@ export const getCharInfoData = async (Name: string, Filter?: string) => {
 export const getCharProfile = async (Name: string) => {
   try {
     const res = await instance.get(`armories/characters/${Name}/profiles`)
-    console.log(`✅ ${Name} 프로필 호출:`, res.data)
+    console.log(`✅ ${Name} 프로필 호출`)
     return res.data
   } catch (error) {
     console.error(`❌ ${Name} 프로필 호출 실패:`, error)
