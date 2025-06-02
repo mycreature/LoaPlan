3e72ad9afa6c3e5dcb349928bf37717b3899e21f
Minseop Kim <aistjq1691@gmail.com>
2025-06-02 20:31:16 +0900

    [feat] 페이지 템플릿 추가

---
+++ Client/src/layouts/DefaultLayout.tsx
@@ -0,0 +1,11 @@
+const DefaultLayout = () => {
+  return (
+    <div className='min-h-screen bg-gray-600'>
+      <main className='space-y-[10px] p-[10px]'>
+        {/* This is where the main content will be rendered */}
+      </main>
+    </div>
+  )
+}
+
+export default DefaultLayout
