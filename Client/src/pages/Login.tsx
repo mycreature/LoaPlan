8f81683b2cc909541ce3d2575fc2f4ce45560e97
Minseop Kim <aistjq1691@gmail.com>
2025-06-02 21:14:37 +0900

    [feat] login 페이지 디자인 구현 및 register 페이지 버튼 텍스트 폰트 변경

--- Client/src/pages/Login.tsx
+++ Client/src/pages/Login.tsx
@@ -2,24 +2,20 @@ import Block from '../components/ui/Block'
 import Button from '../components/ui/Button'
 import Input from '../components/ui/Input'
 
-const register = () => {
+const login = () => {
   return (
     <div className='min-h-screen bg-gray-600'>
       <main className='space-y-[10px] p-[10px]'>
         <div className='flex justify-center gap-x-[10px]'>
-          <Block width={390} height={500}>
+          <Block width={390} height={450}>
             <div className='flex h-full flex-col items-center justify-start'>
-              <h2 className='pt-3 text-black'>회원가입</h2>
+              <h2 className='pt-3 text-black'>로그인</h2>
               <div className='mt-12 w-[90%] space-y-[20px]'>
                 <Input placeholder='이메일' className='w-full' />
                 <Input placeholder='비밀번호' className='w-full' />
-                <Input placeholder='비밀번호 재입력' className='w-full' />
-                <div className='flex w-full items-center justify-between'>
-                  <Input placeholder='API KEY' className='' />
-                  <Button text='인증' textStyle='text-xl font-extrabold' />
-                </div>
               </div>
               <div className='mt-8 w-[90%] space-y-[10px]'>
+                <Button text='로그인' className='w-full' textStyle='text-xl font-extrabold' />
                 <Button text='회원가입' className='w-full' textStyle='text-xl font-extrabold' />
                 <Button
                   text='Google 로그인'
@@ -37,4 +33,4 @@ const register = () => {
   )
 }
 
-export default register
+export default login
