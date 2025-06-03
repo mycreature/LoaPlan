156fdd0a775d5c015a8405e271b7336b519be818
Minseop Kim <aistjq1691@gmail.com>
2025-06-03 14:22:17 +0900

    [feat] register 페이지 대표 캐릭터 항목 추가

--- Client/src/pages/Register.tsx
+++ Client/src/pages/Register.tsx
@@ -7,7 +7,7 @@ const register = () => {
     <div className='min-h-screen bg-gray-600'>
       <main className='space-y-[10px] p-[10px]'>
         <div className='flex justify-center gap-x-[10px]'>
-          <Block width={390} height={500}>
+          <Block width={390} height={550}>
             <div className='flex h-full flex-col items-center justify-start'>
               <h2 className='pt-3 text-black'>회원가입</h2>
               <div className='mt-12 w-[90%] space-y-[20px]'>
@@ -18,6 +18,7 @@ const register = () => {
                   <Input placeholder='API KEY' className='' />
                   <Button text='인증' textStyle='text-xl font-extrabold' />
                 </div>
+                <Input placeholder='대표 캐릭터' className='w-full' />
               </div>
               <div className='mt-8 w-[90%] space-y-[10px]'>
                 <Button text='회원가입' className='w-full' textStyle='text-xl font-extrabold' />
