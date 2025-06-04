ad6b54173e355392794a42869968fb0e2480f3f0
Minseop Kim <aistjq1691@gmail.com>
2025-06-04 19:32:45 +0900

    [feat] block 컴포넌트 style 기능 추가

--- Client/src/components/ui/Block.tsx
+++ Client/src/components/ui/Block.tsx
@@ -1,20 +1,22 @@
 import useThemeStore from '../../stores/ThemeStore'
 
 interface BlockProps {
-  width: number
-  height: number
+  width?: number
+  height?: number
   children?: React.ReactNode
+  style?: React.CSSProperties
 }
 
-const Block = ({ width, height, children }: BlockProps) => {
+const Block = ({ width, height, children, style }: BlockProps) => {
   const { darkMode } = useThemeStore()
 
   return (
     <div
       className={`flex items-center justify-center rounded-lg ${darkMode ? 'bg-gray' : 'bg-white'}`}
       style={{
-        width: `${width}px`,
-        height: `${height}px`,
+        ...style,
+        width: width ? `${width}px` : undefined,
+        height: height ? `${height}px` : undefined,
         flexShrink: 0,
       }}
     >
