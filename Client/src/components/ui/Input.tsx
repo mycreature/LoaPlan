26dd65a4d7df1296a9f782c4d38453e643db2b78
Minseop Kim <aistjq1691@gmail.com>
2025-06-04 19:33:43 +0900

    [feat] input error 기능 추가 (에러시 테두리 색상 변환) 및 disabled 기능 추가

--- Client/src/components/ui/Input.tsx
+++ Client/src/components/ui/Input.tsx
@@ -8,6 +8,8 @@ interface InputProps {
   placeholder?: string
   type?: string
   className?: string
+  error?: string
+  disabled?: boolean
 }
 
 const Input = ({
@@ -17,6 +19,8 @@ const Input = ({
   placeholder = '',
   type = 'text',
   className = '',
+  error = '',
+  disabled = false,
 }: InputProps) => {
   const { darkMode } = useThemeStore()
 
@@ -27,7 +31,8 @@ const Input = ({
       onBlur={onBlur}
       onChange={onChange}
       placeholder={placeholder}
-      className={`rounded-lg border-[1px] bg-none px-3 py-2 text-black ${darkMode ? 'border-black' : 'border-gray'} ${className}`}
+      disabled={disabled}
+      className={`rounded-lg border-[1px] bg-none px-3 py-2 text-black ${!error ? (darkMode ? 'border-black' : 'border-gray') : 'border-red'} ${className}`}
     />
   )
 }
