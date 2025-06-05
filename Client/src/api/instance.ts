faaba4c30e87b17fd93f3c42450289d6f1c52fb9
Minseop Kim <aistjq1691@gmail.com>
2025-06-05 16:38:44 +0900

    [feay] env -> apikey 매개변수로 변환 및 api 응답 콘솔 추가

--- Client/src/api/instance.ts
+++ Client/src/api/instance.ts
@@ -1,23 +1,49 @@
 import axios from 'axios'
 
-const accessToken = 'bearer ' + import.meta.env.VITE_LOSTARK_API_KEY
-console.log('✅ accessToken:', accessToken)
+export const createLostarkInstance = (apikey: string) => {
+  const accessToken = 'bearer ' + apikey
+  console.log('✅ accessToken:', accessToken)
 
-const instance = axios.create({
-  baseURL: 'https://developer-lostark.game.onstove.com',
-  headers: {
-    'content-type': 'application/json;charset-UTF-8',
-    accept: 'application/json,',
-  },
-})
+  const instance = axios.create({
+    baseURL: 'https://developer-lostark.game.onstove.com',
+    headers: {
+      'content-type': 'application/json;charset-UTF-8',
+      accept: 'application/json',
+    },
+  })
 
-instance.interceptors.request.use(
-  function (config: any) {
-    config.headers!.authorization = accessToken
-    return config
-  },
-  function (error) {
-    return Promise.reject(error)
-  },
-)
-export default instance
+  instance.interceptors.request.use(
+    function (config: any) {
+      console.log('✅ Lostark API 응답')
+      config.headers!.authorization = accessToken
+      return config
+    },
+    function (error) {
+      console.error('❌ Lostark API 요청 오류:', error)
+      return Promise.reject(error)
+    },
+  )
+  return instance
+}
+
+// const accessToken = 'bearer ' + import.meta.env.VITE_LOSTARK_API_KEY
+// console.log('✅ accessToken:', accessToken)
+
+// const instance = axios.create({
+//   baseURL: 'https://developer-lostark.game.onstove.com',
+//   headers: {
+//     'content-type': 'application/json;charset-UTF-8',
+//     accept: 'application/json,',
+//   },
+// })
+
+// instance.interceptors.request.use(
+//   function (config: any) {
+//     config.headers!.authorization = accessToken
+//     return config
+//   },
+//   function (error) {
+//     return Promise.reject(error)
+//   },
+// )
+// export default instance
