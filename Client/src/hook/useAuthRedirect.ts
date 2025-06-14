46807081c00242f0b10fb305413169e1211c634d
Minseop Kim <aistjq1691@gmail.com>
2025-06-14 18:41:19 +0900

    [feat] 접근권한 custom hook 구현 (로그인/게스트/비로그인)

---
+++ Client/src/hook/useAuthRedirect.ts
@@ -0,0 +1,44 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+// 로그인 or 게스트 유저만 접근 가능 (비 로그인 x)
+export const useRequireUserOrGuest = (redirectUrl: string = '/') => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const isLogin = !!localStorage.getItem('token')
+    const isGuest = !!sessionStorage.getItem('guest-storage')
+
+    if (!isLogin && !isGuest) {
+      navigate(redirectUrl)
+    }
+  }, [navigate, redirectUrl])
+}
+
+// 로그인 유저만 접근 가능 (게스트, 비 로그인 x)
+export const useRequireUser = (redirectUrl: string = '/') => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const isLogin = !!localStorage.getItem('token')
+    const isGuest = !!sessionStorage.getItem('guest-storage')
+
+    if (isGuest || !isLogin) {
+      navigate(redirectUrl)
+    }
+  }, [navigate, redirectUrl])
+}
+
+// 비 로그인 유저만 접근 가능 (로그인, 게스트 x)
+export const useRequireNoAuth = (redirectUrl: string = '/') => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const isLogin = !!localStorage.getItem('token')
+    const isGuest = !!sessionStorage.getItem('guest-storage')
+
+    if (isLogin || isGuest) {
+      navigate(redirectUrl)
+    }
+  }, [navigate, redirectUrl])
+}
