16b3530e30a9acb74b670bde5adabe48d81aae95
Minseop Kim <aistjq1691@gmail.com>
2025-06-22 21:00:39 +0900

    [feat] MainCard 캐릭터 이미지 로딩시 로딩 아이콘 추가 (Loading.tsx)

--- Client/src/components/character/MainCard.tsx
+++ Client/src/components/character/MainCard.tsx
@@ -1,11 +1,13 @@
 import { useEffect } from 'react'
 import useAccountStore from '../../stores/AccountStore'
 import { useCharacterStore } from '../../stores/CharacterStore'
+import Loading from '../ui/Loading'
 
 const MainCard = () => {
   const MainCharacter = useCharacterStore((state) => state.MainCharacter)
-  const loadProfileData = useCharacterStore((state) => state.loadProfileData) // 함수명 맞춤
+  const loadProfileData = useCharacterStore((state) => state.loadProfileData)
   const characterName = useAccountStore((state) => state.character)
+  const profileLoading = useCharacterStore((state) => state.profileLoading)
 
   useEffect(() => {
     if (characterName) {
@@ -14,8 +16,17 @@ const MainCard = () => {
   }, [characterName, loadProfileData])
 
   return (
-    <div className='mt-[-20px]'>
-      <img src={MainCharacter?.image} alt={MainCharacter?.name || '로딩중... '} />
+    <div className='mt-[-20px] flex h-40 items-center justify-center'>
+      {profileLoading ? (
+        // ✅ 로딩 중일 때
+        <Loading />
+      ) : MainCharacter?.image ? (
+        // ✅ 이미지 로드 완료 시
+        <img src={MainCharacter.image} alt={MainCharacter.name} />
+      ) : (
+        // ✅ 에러시
+        <span></span>
+      )}
     </div>
   )
 }
---
+++ Client/src/components/ui/Loading.tsx
@@ -0,0 +1,7 @@
+const Loading = () => {
+  return (
+    <div className='h-10 w-10 animate-spin rounded-full border-4 border-gray-300 border-t-blue-500' />
+  )
+}
+
+export default Loading
