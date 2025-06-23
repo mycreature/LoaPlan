f800775748877188a578c8c43a20d17d57f4efa0
Minseop Kim <aistjq1691@gmail.com>
2025-06-23 16:38:37 +0900

    [chore] instance.ts의 임시 코드 삭제

--- Client/src/api/instance.ts
+++ Client/src/api/instance.ts
@@ -24,25 +24,3 @@ export const createLostarkInstance = (apikey: string) => {
   )
   return instance
 }
-
-// const accessToken = 'bearer ' + import.meta.env.VITE_LOSTARK_API_KEY
-// console.log('✅ accessToken:', accessToken)
-
-// const instance = axios.create({
-//   baseURL: 'https://developer-lostark.game.onstove.com',
-//   headers: {
-//     'content-type': 'application/json;charset-UTF-8',
-//     accept: 'application/json,',
-//   },
-// })
-
-// instance.interceptors.request.use(
-//   function (config: any) {
-//     config.headers!.authorization = accessToken
-//     return config
-//   },
-//   function (error) {
-//     return Promise.reject(error)
-//   },
-// )
-// export default instance
