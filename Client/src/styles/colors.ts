f33f880dea8526f1b88d157749fb09787251e0b0
Minseop Kim <aistjq1691@gmail.com>
2025-06-25 17:26:47 +0900

    [chore] 색상의 숫자값 제거

--- Client/src/styles/colors.ts
+++ Client/src/styles/colors.ts
@@ -1,8 +1,8 @@
-export const expeditionColors = {
-  1: '#0088FE', // 하늘색
-  2: '#00C49F', // 청록색
-  3: '#FFBB28', // 노란색
-  4: '#FF8042', // 오렌지
-  5: '#A28BFF', // 연보라색
-  6: '#FF6699', // 분홍색
-}
+export const expeditionColors = [
+  '#0088FE', // 하늘색
+  '#00C49F', // 청록색
+  '#FFBB28', // 노란색
+  '#FF8042', // 오렌지
+  '#A28BFF', // 연보라색
+  '#FF6699', // 분홍색
+]
