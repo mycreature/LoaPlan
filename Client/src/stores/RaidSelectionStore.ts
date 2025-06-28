b963428bfa471901baa8094a7bd2f5bc40fb11c3
Minseop Kim <aistjq1691@gmail.com>
2025-06-28 16:03:43 +0900

    [feat] raidSelection를 localstorage내 저장 persist 추가

--- Client/src/stores/RaidSelectionStore.ts
+++ Client/src/stores/RaidSelectionStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 import { CharacterRaidSelection, GateSelection } from '../types/Types'
 
 interface RaidSelectionState {
@@ -13,58 +14,65 @@ interface RaidSelectionState {
   clearAllSelections: () => void
 }
 
-export const useRaidSelectionStore = create<RaidSelectionState>((set, get) => ({
-  characterSelections: [],
+export const useRaidSelectionStore = create<RaidSelectionState>()(
+  persist(
+    (set, get) => ({
+      characterSelections: [],
 
-  toggleGate: (characterName, raidName, type, gate) => {
-    const { characterSelections } = get()
+      toggleGate: (characterName, raidName, type, gate) => {
+        const { characterSelections } = get()
 
-    const character = characterSelections.find((c) => c.characterName === characterName)
+        const character = characterSelections.find((c) => c.characterName === characterName)
 
-    if (character) {
-      const existingRaid = character.selections.find(
-        (s) => s.raidName === raidName && s.type === type,
-      )
+        if (character) {
+          const existingRaid = character.selections.find(
+            (s) => s.raidName === raidName && s.type === type,
+          )
 
-      let updatedSelections: GateSelection[]
+          let updatedSelections: GateSelection[]
 
-      if (existingRaid) {
-        const gates = existingRaid.gates.includes(gate)
-          ? existingRaid.gates.filter((g) => g !== gate) // 선택 해제
-          : [...existingRaid.gates, gate] // 선택 추가
+          if (existingRaid) {
+            const gates = existingRaid.gates.includes(gate)
+              ? existingRaid.gates.filter((g) => g !== gate) // 선택 해제
+              : [...existingRaid.gates, gate] // 선택 추가
 
-        updatedSelections =
-          gates.length > 0
-            ? character.selections.map((s) =>
-                s.raidName === raidName && s.type === type ? { ...s, gates } : s,
-              )
-            : character.selections.filter((s) => !(s.raidName === raidName && s.type === type))
-      } else {
-        updatedSelections = [...character.selections, { raidName, type, gates: [gate] }]
-      }
+            updatedSelections =
+              gates.length > 0
+                ? character.selections.map((s) =>
+                    s.raidName === raidName && s.type === type ? { ...s, gates } : s,
+                  )
+                : character.selections.filter((s) => !(s.raidName === raidName && s.type === type))
+          } else {
+            updatedSelections = [...character.selections, { raidName, type, gates: [gate] }]
+          }
 
-      set({
-        characterSelections: characterSelections.map((c) =>
-          c.characterName === characterName ? { ...c, selections: updatedSelections } : c,
-        ),
-      })
-    } else {
-      set({
-        characterSelections: [
-          ...characterSelections,
-          { characterName, selections: [{ raidName, type, gates: [gate] }] },
-        ],
-      })
-    }
-  },
+          set({
+            characterSelections: characterSelections.map((c) =>
+              c.characterName === characterName ? { ...c, selections: updatedSelections } : c,
+            ),
+          })
+        } else {
+          set({
+            characterSelections: [
+              ...characterSelections,
+              { characterName, selections: [{ raidName, type, gates: [gate] }] },
+            ],
+          })
+        }
+      },
 
-  clearSelectionsForCharacter: (characterName) => {
-    set({
-      characterSelections: get().characterSelections.filter(
-        (c) => c.characterName !== characterName,
-      ),
-    })
-  },
+      clearSelectionsForCharacter: (characterName) => {
+        set({
+          characterSelections: get().characterSelections.filter(
+            (c) => c.characterName !== characterName,
+          ),
+        })
+      },
 
-  clearAllSelections: () => set({ characterSelections: [] }),
-}))
+      clearAllSelections: () => set({ characterSelections: [] }),
+    }),
+    {
+      name: 'raid-selection-storage',
+    },
+  ),
+)
