2a9bdbedfb3d32f226c6d586ee3d01820b00edd3
Minseop Kim <aistjq1691@gmail.com>
2025-06-30 20:58:16 +0900

    [feat] Mobile Layout 내 UI 설정 및 컴포넌트 삽입

--- Client/src/layouts/HomeMobileLayout.tsx
+++ Client/src/layouts/HomeMobileLayout.tsx
@@ -1,8 +1,17 @@
 import Block from '../components/ui/Block'
 
 import MainInfo from '../components/character/MainInfo'
+import BarrackList from '../components/barracks/BarrackList'
+import { getGoldByLevelRange } from '../utils/expeditionDataUtils'
+import { useExpeditionGoldData } from '../hook/useCharacterGoldData'
+import PieChartComponent from '../components/charts/PieChart'
+import { expeditionColors } from '../styles/colors'
+import LevelRangeList from '../components/barracks/LevelRangeList'
+import GoldDashboard from '../components/GoldDashboard'
 
 const HomeMobileLayout = () => {
+  const expeditionGoldData = getGoldByLevelRange(useExpeditionGoldData() || [])
+
   return (
     <main className='space-y-[10px] p-[10px]'>
       <div className='grid grid-cols-1 place-items-center gap-y-[10px]'>
@@ -11,9 +20,35 @@ const HomeMobileLayout = () => {
             <MainInfo />
           </Block>
         </div>
-        {/* 배럭 리스트 (모바일) */}
         <div className='배럭 리스트'>
-          <Block width={370} height={300} />
+          <Block width={370} height={300}>
+            <BarrackList />
+          </Block>
+        </div>
+        <div className='차트'>
+          <Block width={370} height={230}>
+            <PieChartComponent
+              data={expeditionGoldData}
+              colors={expeditionColors}
+              width={200}
+              height={200}
+              outerRadius={100}
+            />
+          </Block>
+        </div>
+        <div className='LevelRangeList'>
+          <Block width={370} height={220}>
+            <div className='my-auto'>
+              <LevelRangeList />
+            </div>
+          </Block>
+        </div>
+        <div className='GoldDashboard'>
+          <Block width={370} height={220}>
+            <div className='my-auto'>
+              <GoldDashboard />
+            </div>
+          </Block>
         </div>
       </div>
     </main>
