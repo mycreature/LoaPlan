20aedcb44cb19e14d70ceef1ed4ae2bbff42310a
Minseop Kim <aistjq1691@gmail.com>
2025-07-02 22:42:34 +0900

    [feat] MainInfo (home 내 이미지 캐릭터의 각종 정보) 를 Account의 character -> 사용자가 선택한 SelectionCharacter 기준으로 변경 및 에러처리

--- Client/src/components/character/MainInfo.tsx
+++ Client/src/components/character/MainInfo.tsx
@@ -1,4 +1,5 @@
 import { useCharacterStore } from '../../stores/api/CharacterStore'
+import { useCharacterSelectionStore } from '../../stores/selections/CharacterSelectionStore'
 import Loading from '../ui/Loading'
 import Tag from '../ui/Tag'
 
@@ -6,6 +7,43 @@ const MainInfo = () => {
   const MainCharacter = useCharacterStore((state) => state.MainCharacter)
   const profileLoading = useCharacterStore((state) => state.profileLoading)
 
+  const SelectedCharacterInfo = useCharacterSelectionStore((state) => state.SelectedCharacterInfo)
+  const SelectedProfileError = useCharacterSelectionStore((state) => state.profileError)
+
+  if (SelectedProfileError) return <div className='text-center text-black'>정보 불러오기 실패</div>
+
+  if (SelectedCharacterInfo) {
+    return (
+      <div className='w-[80%]'>
+        <div className='items-center justify-center space-y-6'>
+          <div className='flex w-full items-center space-x-6'>
+            <Tag text='닉네임' className='w-[100px]'></Tag>
+            <h3 className='text-black'>{SelectedCharacterInfo.name}</h3>
+          </div>
+          <div className='flex w-full items-center space-x-6'>
+            <Tag text='서버' className='w-[100px]'></Tag>
+            <h3 className='text-black'>{SelectedCharacterInfo.server}</h3>
+          </div>
+          <div className='flex w-full items-center space-x-6'>
+            <Tag text='클래스' className='w-[100px]'></Tag>
+            <h3 className='text-black'>{SelectedCharacterInfo.class}</h3>
+          </div>
+          <div className='flex w-full items-center space-x-6'>
+            <Tag text='레벨' className='w-[100px]'></Tag>
+            <h3 className='text-black'>{Number(SelectedCharacterInfo.level?.replace(/,/g, ''))}</h3>
+          </div>
+          <div className='flex w-full items-center space-x-6'>
+            <Tag text='전투레벨' className='w-[100px]'></Tag>
+            <h3 className='text-black'>{Number(SelectedCharacterInfo.CharacterLevel)}</h3>
+          </div>
+          <div className='flex w-full items-center space-x-6'>
+            <Tag text='전투력' className='w-[100px]'></Tag>
+            <h3 className='text-black'>{SelectedCharacterInfo.combatPower}</h3>
+          </div>
+        </div>
+      </div>
+    )
+  }
   return (
     <div className='w-[80%]'>
       {profileLoading && !MainCharacter ? (
@@ -15,7 +53,7 @@ const MainInfo = () => {
         </div>
       ) : MainCharacter?.image ? (
         // ✅ 이미지 로드 완료 시
-        <div className='items-center justify-center space-y-5'>
+        <div className='items-center justify-center space-y-6'>
           <div className='flex w-full items-center space-x-6'>
             <Tag text='닉네임' className='w-[100px]'></Tag>
             <h3 className='text-black'>{MainCharacter.name}</h3>
@@ -31,6 +69,10 @@ const MainInfo = () => {
           <div className='flex w-full items-center space-x-6'>
             <Tag text='레벨' className='w-[100px]'></Tag>
             <h3 className='text-black'>{Number(MainCharacter.level?.replace(/,/g, ''))}</h3>
+          </div>
+          <div className='flex w-full items-center space-x-6'>
+            <Tag text='전투레벨' className='w-[100px]'></Tag>
+            <h3 className='text-black'>{Number(MainCharacter.CharacterLevel)}</h3>
           </div>
           <div className='flex w-full items-center space-x-6'>
             <Tag text='전투력' className='w-[100px]'></Tag>
