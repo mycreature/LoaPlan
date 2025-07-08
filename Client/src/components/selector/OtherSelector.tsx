252947465890b0b9bb02cf4486a789aa5ba640f0
Minseop Kim <aistjq1691@gmail.com>
2025-07-08 16:34:12 +0900

    [chore] otherSelection toggleDrop 테스트 버튼 추가

--- Client/src/components/selector/OtherSelector.tsx
+++ Client/src/components/selector/OtherSelector.tsx
@@ -1,23 +1,40 @@
 import useThemeStore from '../../stores/others/ThemeStore'
 import { useOtherSelectionStore } from '../../stores/selections/OtherSelectionStore'
 import { getAvailableOthersByLevel } from '../../utils/expeditionDataUtils'
+import Button from '../ui/Button'
 
 const OtherSelector = ({ SelectedCharacterInfo }: { SelectedCharacterInfo: any }) => {
   const darkMode = useThemeStore((state) => state.darkMode)
-  const { characterSelections } = useOtherSelectionStore()
+  const { toggleDrop, characterSelections } = useOtherSelectionStore()
 
-  console.log('characterSelections', characterSelections)
+  console.log(characterSelections)
 
   const availableOther = getAvailableOthersByLevel(
     parseFloat(SelectedCharacterInfo?.level.replace(/,/g, '') || '0'),
   )
 
-  console.log('availableOther', availableOther)
-
   return (
     <div
       className={`${darkMode ? 'border-black/20' : 'border-gray'} flex w-full flex-col gap-2 rounded-xl border-2 p-[10px]`}
-    ></div>
+    >
+      {Object.entries(availableOther).map(([key, value]) => (
+        <div className='flex justify-between p-[10px]'>
+          <h4 className='text-black'>{value.name}</h4>
+          <Button
+            text='선택'
+            onClick={() =>
+              toggleDrop(
+                SelectedCharacterInfo.name,
+                value.name,
+                value.type,
+                value.level,
+                value.drops[Number(key)],
+              )
+            }
+          />
+        </div>
+      ))}
+    </div>
   )
 }
 
