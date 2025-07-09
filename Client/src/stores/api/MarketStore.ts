8d6970ac4a3a0e2fdc71062589a710e9eb2c4530
Minseop Kim <aistjq1691@gmail.com>
2025-07-09 21:25:07 +0900

    [feat] 마켓 아이템정보 값을 zustand store 저장시 res.Name (아이템 이름) 로 구분지어 배열 형태로 선언

--- Client/src/stores/api/MarketStore.ts
+++ Client/src/stores/api/MarketStore.ts
@@ -7,24 +7,28 @@ interface MarketState {
 }
 
 export const useMarketStore = create<MarketState>((set) => ({
+  itemInfos: {},
+
   loadItemInfo: async (CategoryCode: number, itemName?: string, pageNo?: number) => {
     try {
       set({ MarketLoading: true, MarketError: null })
 
       const apiKey = getApiKey()
-
       const res = await getItemData(apiKey, CategoryCode, itemName, pageNo)
       if (res) {
-        set({
-          itemInfo: {
-            name: res.Name,
-            image: res.Icon,
-            BundleCount: res.BundleCount,
-            YDayAvgPrice: res.YDayAvgPrice,
-            RecentPrice: res.RecentPrice,
-            CurrentMinPrice: res.CurrentMinPrice,
+        set((state) => ({
+          itemInfos: {
+            ...state.itemInfos,
+            [res.Name]: {
+              name: res.Name,
+              image: res.Icon,
+              BundleCount: res.BundleCount,
+              YDayAvgPrice: res.YDayAvgPrice,
+              RecentPrice: res.RecentPrice,
+              CurrentMinPrice: res.CurrentMinPrice,
+            },
           },
-        })
+        }))
       }
     } catch (error) {
       set({ MarketError: error instanceof Error ? error.message : '아이템 정보 불러오기 실패' })
@@ -33,13 +37,5 @@ export const useMarketStore = create<MarketState>((set) => ({
     }
   },
 
-  // 특정 프로필 요소 선택
-  selectProfileElement: (element: any) => {
-    set({ selectedProfileElement: element })
-  },
-
-  // 상태 초기화
-  resetState: () => {
-    set({})
-  },
+  resetState: () => set({ itemInfos: {} }),
 }))
