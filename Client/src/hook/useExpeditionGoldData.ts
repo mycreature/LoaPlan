67ac3bcac13b918751ffa814bff657eb6f3b1242
Minseop Kim <aistjq1691@gmail.com>
2025-07-10 21:40:27 +0900

    [feat] ExpeditionGoldData내에 gold값이 기타컨텐츠 (OtherSelection) 의 드랍테이블 추가 및 골드로 변환해 종합 골드에 합산 추가

--- Client/src/hook/useExpeditionGoldData.ts
+++ Client/src/hook/useExpeditionGoldData.ts
@@ -1,17 +1,24 @@
+import { useMarketStore } from './../stores/api/MarketStore'
 import { useExpeditionStore } from '../stores/api/ExpeditionStore'
+import { useOtherSelectionStore } from '../stores/selections/OtherSelectionStore'
 import { useRaidSelectionStore } from '../stores/selections/RaidSelectionStore'
-import { getRaidGoldForCharacter } from '../utils/SelectionUtils'
+import { getOtherDropsForCharacter, getRaidGoldForCharacter } from '../utils/SelectionUtils'
+import { calculateGoldByDrops } from '../utils/MarketDataUtils'
 
 // 사용자가 선택한 selections 값을 expeditions(원정대) 에 맞춰 배열로 변환하는 훅
 export const useExpeditionGoldData = () => {
   const RaidSelections = useRaidSelectionStore((state) => state.characterSelections)
+  const OtherSelection = useOtherSelectionStore((state) => state.characterSelections)
   const expeditions = useExpeditionStore((state) => state.expeditions)
+  const itemInfos = useMarketStore((state) => state.itemInfos)
 
   if (!expeditions) return []
 
   return expeditions.map((character: { name: string; level: string }) => ({
     name: character.name,
     level: character.level,
-    gold: getRaidGoldForCharacter(character.name, RaidSelections),
+    gold:
+      getRaidGoldForCharacter(character.name, RaidSelections) +
+      calculateGoldByDrops(getOtherDropsForCharacter(character.name, OtherSelection), itemInfos),
   }))
 }
