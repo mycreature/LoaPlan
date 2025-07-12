df9df6ac0002c3074215dd9bc8ddcd1ff579b9eb
Minseop Kim <aistjq1691@gmail.com>
2025-07-12 17:15:54 +0900

    [feat] otherSelector 의 버튼 -> checkbox 변경 및 선택 여부 확인 함수 추가

--- Client/src/components/selector/OtherSelector.tsx
+++ Client/src/components/selector/OtherSelector.tsx
@@ -1,37 +1,55 @@
 import useThemeStore from '../../stores/others/ThemeStore'
 import { useOtherSelectionStore } from '../../stores/selections/OtherSelectionStore'
 import { getAvailableOthersByLevel } from '../../utils/expeditionDataUtils'
-import Button from '../ui/Button'
+import Checkbox from '../ui/CheckBox'
 
 const OtherSelector = ({ SelectedCharacterInfo }: { SelectedCharacterInfo: any }) => {
   const darkMode = useThemeStore((state) => state.darkMode)
-  const { toggleDrops } = useOtherSelectionStore()
+  const { toggleDrops, characterSelections } = useOtherSelectionStore()
 
   const availableOther = getAvailableOthersByLevel(
     parseFloat(SelectedCharacterInfo?.level.replace(/,/g, '') || '0'),
   )
 
+  // 선택 여부 확인 함수
+  const isOtherSelected = (name: string) => {
+    if (!SelectedCharacterInfo) return false
+
+    const character = characterSelections.find(
+      (c) => c.characterName === SelectedCharacterInfo.name,
+    )
+    if (!character) return false
+
+    return character.selections.some((s) => s.name === name)
+  }
+
   return (
     <div
-      className={`${darkMode ? 'border-black/20' : 'border-gray'} flex w-full flex-col gap-2 rounded-xl border-2 p-[10px]`}
+      className={`${
+        darkMode ? 'border-black/20' : 'border-gray'
+      } flex w-full flex-col gap-2 rounded-xl border-2 p-[10px]`}
     >
-      {Object.entries(availableOther).map(([key, value]) => (
-        <div key={key} className='flex justify-between p-[10px]'>
-          <h4 className='text-black'>{value.name}</h4>
-          <Button
-            text='선택'
-            onClick={() =>
-              toggleDrops(
-                SelectedCharacterInfo.name,
-                value.name,
-                value.type,
-                value.level,
-                value.drops,
-              )
-            }
-          />
-        </div>
-      ))}
+      {Object.entries(availableOther).map(([key, value]) => {
+        const selected = isOtherSelected(value.name)
+
+        return (
+          <div key={key} className='flex items-center justify-between p-[10px]'>
+            <h4 className='text-black'>{value.name}</h4>
+            <Checkbox
+              checked={selected}
+              onChange={() =>
+                toggleDrops(
+                  SelectedCharacterInfo.name,
+                  value.name,
+                  value.type,
+                  value.level,
+                  value.drops,
+                )
+              }
+            />
+          </div>
+        )
+      })}
     </div>
   )
 }
