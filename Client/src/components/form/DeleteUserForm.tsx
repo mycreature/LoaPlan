a6539a942c58299947d78eb26e3c6c5ce7afac52
Minseop Kim <aistjq1691@gmail.com>
2025-07-28 22:48:03 +0900

    [feat] 회원 탈퇴 기능 구현 (DeleteUserForm 컴포넌트 추가) (submit 없는 Form 사용 이유: userinfo 회원 수정과 동일한 UI/UX 일관성 및 시맨틱 구조 유지)

---
+++ Client/src/components/form/DeleteUserForm.tsx
@@ -0,0 +1,43 @@
+import { useNavigate } from 'react-router-dom'
+import { requestDeleteUser } from '../../api/userApi'
+import useAccountStore from '../../stores/others/AccountStore'
+import Button from '../ui/Button'
+
+const DeleteUserForm = () => {
+  const navigate = useNavigate()
+  const email = useAccountStore((state) => state.email)
+
+  const handleDeleteUser = async () => {
+    const confirmDelete = window.confirm('정말로 탈퇴하시겠습니까?')
+    if (!confirmDelete) return
+
+    if (!email) {
+      alert('이메일 정보가 없습니다. 다시 로그인해주세요.')
+      return
+    }
+    try {
+      await requestDeleteUser(email)
+      navigate('/login')
+    } catch (error) {
+      console.error('회원 탈퇴 실패:', error)
+      alert('회원 탈퇴 중 오류가 발생했습니다.')
+    } finally {
+      window.location.reload()
+      alert('회원 탈퇴가 완료되었습니다.')
+    }
+  }
+
+  return (
+    <div className='flex flex-col'>
+      <Button
+        type='button'
+        text='회원 탈퇴'
+        onClick={handleDeleteUser}
+        lightColor='bg-red'
+        darkColor='bg-black'
+      />
+    </div>
+  )
+}
+
+export default DeleteUserForm
