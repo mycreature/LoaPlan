047e230e85950d1d30cd9cb7d08100eba87671cc
Minseop Kim <aistjq1691@gmail.com>
2025-07-31 00:41:41 +0900

    [fix]  form onSubmit 방식으로 변경하고 새로고침 방지 로직 추가

--- Client/src/components/form/GusetLoginForm.tsx
+++ Client/src/components/form/GusetLoginForm.tsx
@@ -7,7 +7,13 @@ interface GuestLoginFormProps {
 
 const GuestLoginForm = ({ onSubmit, isLoading = false }: GuestLoginFormProps) => {
   return (
-    <form onSubmit={onSubmit} className='flex flex-col'>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault() // 새로고침 방지
+        onSubmit()
+      }}
+      className='flex flex-col'
+    >
       <Button
         text={isLoading ? '로그인 중...' : '게스트 로그인'}
         type='submit'
