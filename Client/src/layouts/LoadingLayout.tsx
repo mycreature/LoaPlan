0b2b7468b4515f05ab38f1c15b6d1be4098ea0a0
Minseop Kim <aistjq1691@gmail.com>
2025-07-30 18:26:24 +0900

    [feat] 뷰포인트값이 초기값 (null) 일시 임시 로딩페이지 구현

---
+++ Client/src/layouts/LoadingLayout.tsx
@@ -0,0 +1,11 @@
+import Loading from '../components/ui/Loading'
+
+const LoadingLayout = () => {
+  return (
+    <main className='flex h-screen w-full items-center justify-center'>
+      <Loading />
+    </main>
+  )
+}
+
+export default LoadingLayout
