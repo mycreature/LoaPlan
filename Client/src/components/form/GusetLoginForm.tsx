4cefb4544df62dd7a2c6524ae0432cffc5ddd356
Minseop Kim <aistjq1691@gmail.com>
2025-07-30 17:44:54 +0900

    [feat] GusetLoginForm 구현

---
+++ Client/src/components/form/GusetLoginForm.tsx
@@ -0,0 +1,22 @@
+import Button from '../ui/Button'
+
+interface GuestLoginFormProps {
+  onSubmit: any
+  isLoading: boolean
+}
+
+const GuestLoginForm = ({ onSubmit, isLoading = false }: GuestLoginFormProps) => {
+  return (
+    <form onSubmit={onSubmit} className='flex flex-col'>
+      <Button
+        text={isLoading ? '로그인 중...' : '게스트 로그인'}
+        type='submit'
+        disabled={isLoading}
+        darkColor='bg-black'
+        lightColor='bg-red'
+      />
+    </form>
+  )
+}
+
+export default GuestLoginForm
