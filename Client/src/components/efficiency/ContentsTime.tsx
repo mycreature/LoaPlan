4ef386a15de02f742191cbf219e0d4078d8d7285
Minseop Kim <aistjq1691@gmail.com>
2025-09-08 19:47:06 +0900

    [refactor] ContentsTime 컴포넌트 리팩토링 및 UI 개선 (darkmode, map() 코드중복 제거)

--- Client/src/components/efficiency/ContentsTime.tsx
+++ Client/src/components/efficiency/ContentsTime.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-import { useContentsTimeStore } from '../../stores/selections/ContentsTimeSelectionStore'
 import { EfficiencyInput } from './efficiencyInput'
 import Checkbox from '../ui/CheckBox'
+import useThemeStore from '../../stores/others/ThemeStore'
+import { useContentsTimeStore } from '../../stores/selections/ContentsTimeSelectionStore'
 
 export const ContentsTime = () => {
   const {
@@ -14,41 +15,71 @@ export const ContentsTime = () => {
     toggleFrontline,
   } = useContentsTimeStore()
 
-  // 로컬 시간 변경 함수 (레이드, 전선, 가토, 휴게 여부)
-  const [localRaid, setLocalTime] = useState<number>(30)
+  const darkMode = useThemeStore((state) => state.darkMode)
+
+  const [localRaid, setLocalRaid] = useState<number>(30) //
   const [localGuardian, setLocalGuardian] = useState<number>(3)
   const [localFrontline, setLocalFrontline] = useState<number>(3)
 
+  // 각 UI 블록 필요한 데이터를 배열로 정의합니다.
+  const contentData = [
+    {
+      id: 'raid',
+      label: '레이드 클리어 시간 :',
+      value: localRaid,
+      onChange: setLocalRaid,
+      onBlur: setRaidTime,
+      hasCheckbox: false, // 휴게 체크박스 유무
+    },
+    {
+      id: 'frontline',
+      label: '전선 클리어 시간 :',
+      value: localFrontline,
+      onChange: setLocalFrontline,
+      onBlur: setFrontlineTime,
+      hasCheckbox: true,
+      isChecked: isFrontline,
+      onToggle: toggleFrontline,
+    },
+    {
+      id: 'guardian',
+      label: '가토 클리어 시간 :',
+      value: localGuardian,
+      onChange: setLocalGuardian,
+      onBlur: setGuardianTime,
+      hasCheckbox: true,
+      isChecked: isGuardian,
+      onToggle: toggleGuardian,
+    },
+  ]
+
+  const commonDivClass = `${darkMode ? 'border-black/20' : 'border-gray'} flex items-center justify-between rounded-lg border-3 px-3 py-2`
+
   return (
-    <div className='flex items-center justify-start gap-3'>
-      <div className='border-gray flex items-center justify-between rounded-lg border-3 px-3 py-2'>
-        <h4 className='text-black'>레이드 클리어 시간 : </h4>
-        <EfficiencyInput value={localRaid} onChange={setLocalTime} onBlur={setRaidTime} />
-      </div>
-      <div className='border-gray flex items-center justify-between rounded-lg border-3 px-3 py-2'>
-        <h4 className='text-black'>전선 클리어 시간 : </h4>
-        <EfficiencyInput
-          value={localFrontline}
-          onChange={setLocalFrontline}
-          onBlur={setFrontlineTime}
-        />
-        <div className='flex items-center gap-2'>
-          <h4 className='text-black'>휴게: </h4>
-          <Checkbox checked={isFrontline} onChange={toggleFrontline} width='24px' height='24px' />
+    <div className='flex items-start justify-start gap-3 max-md:flex-col'>
+      {/* 데이터 배열을 map으로 순회하여 UI를 렌더링 */}
+      {contentData.map((content) => (
+        <div key={content.id} className={commonDivClass}>
+          <h4 className='text-black'>{content.label}</h4>
+          <EfficiencyInput
+            value={content.value}
+            onChange={content.onChange}
+            onBlur={content.onBlur}
+          />
+          {/* 휴게 체크박스를 조건부 렌더링 */}
+          {content.hasCheckbox && (
+            <div className='flex items-center gap-2'>
+              <h4 className='text-black'>휴게: </h4>
+              <Checkbox
+                checked={content.isChecked}
+                onChange={content.onToggle}
+                width='24px'
+                height='24px'
+              />
+            </div>
+          )}
         </div>
-      </div>
-      <div className='border-gray flex items-center justify-between rounded-lg border-3 px-3 py-2'>
-        <h4 className='text-black'>가토 클리어 시간 : </h4>
-        <EfficiencyInput
-          value={localGuardian}
-          onChange={setLocalGuardian}
-          onBlur={setGuardianTime}
-        />
-        <div className='flex items-center gap-2'>
-          <h4 className='text-black'>휴게: </h4>
-          <Checkbox checked={isGuardian} onChange={toggleGuardian} width='24px' height='24px' />
-        </div>
-      </div>
+      ))}
     </div>
   )
 }
