791e10341a81e881209c05cc75b96af49108c448
mycreature <aistjq1691@gmail.com>
2025-09-12 23:41:57 +0900

    [feat] Header 내 FAQ 버튼 및 기능 구현

--- Client/src/components/etc/FAQ.tsx
+++ Client/src/components/etc/FAQ.tsx
@@ -2,41 +2,63 @@ import { useState } from 'react'
 import Block from '../ui/Block'
 import useThemeStore from '../../stores/others/ThemeStore'
 
-// 1. FAQ 데이터를 컴포넌트 밖의 배열로 분리하여 관리 용이성 증대
+// velog 주소 데이터
+const urlData = [
+  {
+    url: 'https://velog.io/@aistjq1691/LOSTARK-OPEN-API-KEY-%EB%B0%9C%EA%B8%89-%EB%B0%A9%EB%B2%95',
+    title: 'LOSTARK API-Key 생성 방법',
+  },
+
+  {
+    url: 'https://velog.io/@aistjq1691/LOAPLAN-%EC%82%AC%EC%9A%A9-%EA%B0%80%EC%9D%B4%EB%93%9C-gut24twi',
+    title: 'LOAPLAN 사용 가이드',
+  },
+]
+
+// FAQ 데이터
 const FAQData = [
   {
     question: 'API 발급은 어떻게 하나요?',
     answer: (
-      <a href='/docs/api' className='text-blue-600 hover:underline dark:text-blue-400'>
-        API 문서 페이지에서 발급받을 수 있습니다.
+      <a
+        href={urlData[0].url}
+        target='_blank'
+        className='text-blue-600 hover:underline dark:text-blue-400'
+      >
+        LOSTARK API 사이트에서 발급받을 수 있습니다.
       </a>
     ),
   },
   {
     question: '어떻게 사용하는지 모르겠어요.',
     answer: (
-      <a href='/guide' className='text-blue-600 hover:underline dark:text-blue-400'>
-        사용 가이드 페이지를 참고해주세요.
+      <a
+        href={urlData[1].url}
+        target='_blank'
+        className='text-blue-600 hover:underline dark:text-blue-400'
+      >
+        사용 가이드를 참고해주세요.
       </a>
     ),
   },
   {
     question: '다른 기기에 접속시 저장한 내용이 없어집니다.',
-    answer:
-      '현재 저장 방식은 "브라우저" 기준이기에  다른 디바이스에 접속 시 기존 정보가 보이지 않습니다. 빠른 시일 내 개선하겠습니다.',
+    answer: `현재 저장 방식은 "브라우저" 기준이기에  다른 
+    디바이스에 접속 시 기존 정보가 보이지 않습니다.
+    빠른 시일 내 개선하겠습니다.`,
   },
   {
     question: '사용 중 불편하거나 개선했으면 하는 점이 있어요.',
     answer: (
       <>
-        서비스 운영은 처음이라 미숙한 부분이 많습니다 ㅠㅠ{' '}
+        서비스 운영은 처음이라 미숙한 부분이 많습니다 ㅠㅠ [
         <a
           href='mailto:loaplan1234@gmail.com'
           className='text-blue-600 hover:underline dark:text-blue-400'
         >
           loaplan1234@gmail.com
         </a>
-        으로 의견을 보내주시면 감사하겠습니다.
+        ]으로 의견을 보내주시면 감사하겠습니다.
       </>
     ),
   },
@@ -54,7 +76,6 @@ const FAQ = () => {
   return (
     <div>
       <Block title='FAQ' auth={true} width={390}>
-        {/* 이 부분에서 space-y-3 클래스를 제거했습니다. */}
         <div className='w-full'>
           {FAQData.map((item, index) => (
             <div key={index} className='flex flex-col gap-4'>
@@ -70,7 +91,7 @@ const FAQ = () => {
                   openIndex === index ? 'max-h-screen pb-4' : 'max-h-0'
                 }`}
               >
-                <h4 className='text-black'>{item.answer}</h4>
+                <h4 className='whitespace-pre-line text-black'>{item.answer}</h4>
               </div>
             </div>
           ))}
