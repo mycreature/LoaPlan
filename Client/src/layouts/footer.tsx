97e2dbcbeea890feadec3af8a718d45734af7166
Minseop Kim <aistjq1691@gmail.com>
2025-09-20 16:56:48 +0900

    [style] Footer 내 글자 크기 조정

--- Client/src/layouts/footer.tsx
+++ Client/src/layouts/footer.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <footer className='fixed bottom-0 left-0 z-40 flex h-[40px] w-full items-center justify-center bg-black'>
-      <div className='flex gap-5 text-sm text-white'>
+      <div className='flex gap-5 text-xs text-white'>
         <p>© 2025 LOAPLAN. All rights reserved.</p>
         <Link to='/privacy-policy' className='text-white underline'>
           개인정보처리방침
